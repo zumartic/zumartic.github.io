919583422819cb0fba05cd6b65dee64fcb3f495a
Sami Kangas <zumartic@gmail.com>
2017-11-21 18:48:31 +0200

    recipe box added

--- js/index.js
+++ js/index.js
@@ -1,2 +1,16 @@
 $('.navbar-collapse ul li a').click(function() {
   $(".navbar-collapse").collapse('hide');});
+/*
+  window.onscroll = function() {myFunction()};
+  
+  function myFunction() {
+      console.log(document.documentElement.scrollTop);
+      
+      if (document.documentElement.scrollTop > 200) {
+        document.getElementById("About").style.backgroundColor = "#000000";
+      } else {
+        document.getElementById("About").style.backgroundColor = "rgb(255,255,204)";
+      }
+  }
+
+  */
